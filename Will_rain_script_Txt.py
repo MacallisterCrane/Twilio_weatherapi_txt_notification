c470c4cb6c8d1df10270f43c06424484ccef2718
Macallister Crane <macallistercrane@yahoo.com>
2024-10-07 22:37:19 +0000

    Update Will_rain_script_Txt.py

--- Will_rain_script_Txt.py
+++ Will_rain_script_Txt.py
@@ -1,11 +1,11 @@
 import requests
 from twilio.rest import Client
 Weather = "https://api.openweathermap.org/data/2.5/forecast"
-Free_API_Key = "687fec0210cedcc33afc3d3cf3b96b16"
-account_sid = "AC47bd692afb6a3a6f600cc63278568e3c"
-auth_token = "7f46eb3c99316b57695744c83b5f04cb"
+Free_API_Key = "<YOUR WEATHER KEY>"
+account_sid = "<YOUR TWILIO KEY>"
+auth_token = "<YOUR TWILIO AUTH TOKEN>"
 
-
+# Coords currently set for Boise, ID
 Weather_params = {
     "lat": 43.615021,
     "lon": -116.202316,
@@ -26,7 +26,7 @@ if rain:
     client = Client(account_sid, auth_token)
     message = client.messages.create(
         body="Bring an umbrella today!",
-        from_="whatsapp:+14155238886",
-        to="whatsapp:+18316010502",
+        from_="<YOUR TWILIO/WHATSAPP PHONE #>",
+        to="<YOUR VERIFIED PHONE #>",
     )
     print(message.status)
